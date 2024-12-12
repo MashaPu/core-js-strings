149bec7eb864077d7a207c0386641484ce8aba46
MashaPu <mariya_pudovkina@mail.ru>
2024-12-12 10:29:16 +0500

    feat: 19 task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -226,8 +226,8 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  return new Date.toString(minutes, seconds);
 }
 
 /**
@@ -240,8 +240,8 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 /**
@@ -255,8 +255,8 @@ function reverseString(/* str */) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  return str.split('').sort().join('');
 }
 
 /**
@@ -271,8 +271,8 @@ function orderAlphabetically(/* str */) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(/* str, substring */) {
-  throw new Error('Not implemented');
+function containsSubstring(str, substring) {
+  return str.includes(substring);
 }
 
 /**
@@ -289,8 +289,18 @@ function containsSubstring(/* str, substring */) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const arr = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+  // if (typeof str !== 'string') return 0;
+  // return str.replace(/[^aeiouAEIOUY]/gi, '');
+  // return str.split('').reduce((acc, cur) => {
+  //   if (arr.includes(cur)) {
+  //     // const result = acc + 1;
+  //     return acc + 1;
+  //   }
+  //   return acc;
+  // }, 0);
+  return str.split('').filter((item) => arr.includes(item)).length;
 }
 
 /**
